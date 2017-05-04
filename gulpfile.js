cc7b59aeba24a5732bec4818bc795f31fb14bcfe
petrahagman <phagman@Ps-MacBook-Pro.local>
2017-05-04 15:58:32 +0200

    Påbörjat request till IG, obs - ej klart

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp');
-
 // Plugins
 var sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     bs = require('browser-sync').create(),
+    connect = require('gulp-connect-php'),
     babel = require('gulp-babel'),
     babelify = require('babelify'),
     browserify = require('browserify');
-
 // Tasks
 gulp.task('scripts', function() {
   return gulp.src('src/js/**/*.js')
@@ -20,19 +19,24 @@ gulp.task('scripts', function() {
     .pipe(uglify())
     .pipe(gulp.dest('./js'));
 });
-
 //Convert ES6 ode in all js files in src/js folder and copy to 
 //build folder as bundle.js
 gulp.task("build", function(){
     return browserify({
-      entries: ["./js/app.js"]
+      entries: ["./src/js/*.js"]
     })
     .transform(babelify)
     .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("./build"));
+    .pipe(source("all.js"))
+    .pipe(gulp.dest("./js"));
 });
-
+gulp.task('connect-sync', function() {
+    connect.server({}, function (){
+        browserSync({
+          proxy: '127.0.0.1:8000'
+        });
+    });
+});
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
@@ -48,11 +52,10 @@ gulp.task('browser-sync', ['sass'], function() {
   })
 });
 //Default task. This will be run when no task is passed in arguments to gulp
-gulp.task("default", ["build"]);
-
+gulp.task('default', ['build']);
 // Watch
 gulp.task('watch', ['browser-sync'], function() {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/js/**/*.js', ['scripts']);
-  gulp.watch("*.html").on("change", bs.reload);
+gulp.watch('src/scss/**/*.scss', ['sass']);
+gulp.watch('src/js/**/*.js', ['scripts']);
+gulp.watch('*.html').on('change', bs.reload);
 });
