31685125f8fdecad93768418341329fc821951c2
jessica <jessican.niemi@gmail.com>
2017-05-05 15:03:50 +0200

    skriver ut lagom mkt json i filer

--- request.php
+++ request.php
@@ -1,26 +1,37 @@
 <?php
-//Filter on tag #coachellafestival
-$baseurl = 'https://www.instagram.com/explore/tags/coachellafestival/?__a=1';
-$url = $baseurl;
 
-//Get data from Instagram
-$data = json_decode(file_get_contents($url));
+$media = "media";
+$fn_mostrecent = "mostrecent";
+$topposts = "top_posts";
+$fn_toplist = "toplist";
 
-//Get most recent posts
-for ($i = 0; $i < 10; $i++) {
-    $mostRecent = $data->tag->media->nodes[$i];
+
+function saveJson($filter, $filename) {
+    //Filter on tag #coachellafestival
+    $baseurl = 'https://www.instagram.com/explore/tags/coachellafestival/?__a=1';
+    $url = $baseurl;
+    //Get data from Instagram
+    $data = json_decode(file_get_contents($url));
+    
+    // get post data
+    $outdata = array();
+    for ($i = 0; $i < 10; $i++) {
+        $outdata[] = $data->tag->$filter->nodes[$i];
+        
+        // Saves the data in json file
+    }
+    var_dump($data);
+    file_put_contents("src/json/".$filename.".json", json_encode($outdata));
 }
+saveJson($media, $fn_mostrecent);
+saveJson($topposts, $fn_toplist);
 
-// Saves the data in json file
-file_put_contents("src/json/mostrecent.json", json_encode($mostRecent));
 
-//Get top posts
-for ($i = 0; $i < 10; $i++) {
-    $topPosts = $data->tag->top_posts->nodes[$i];
-}
 
-// Saves the data in json file
-file_put_contents("src/json/top.json", json_encode($topPosts));
+
+
+
+
 
 ?>
 
