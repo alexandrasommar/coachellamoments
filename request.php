0d719552e48f6e027ecf835438e35cb5b3b404d1
Alexandra Sommar <alexandra@sommar.se>
2017-05-09 14:46:47 +0200

    Visar bara bilder obs buggigt

--- request.php
+++ request.php
@@ -12,13 +12,17 @@ function saveJson($filter, $filename) {
     $url = $baseurl;
     //Get data from Instagram
     $data = json_decode(file_get_contents($url));
-    
+
     // get post data
     $outdata = array();
-    for ($i = 0; $i < 10; $i++) {
-        $outdata[] = $data->tag->$filter->nodes[$i];
-        
-        // Saves the data in json file
+
+      for ($i = 0; $i < count($data->tag->$filter->nodes); $i++) {
+        $video = $data->tag->$filter->nodes[$i]->is_video;
+        var_dump($video);
+          if(!$video) {
+            $outdata[] = $data->tag->$filter->nodes[$i];
+          }
+          // Saves the data in json file
     }
     var_dump($data);
     file_put_contents("src/json/".$filename.".json", json_encode($outdata));
@@ -34,8 +38,3 @@ saveJson($topposts, $fn_toplist);
 
 
 ?>
-
-
-
-
-
