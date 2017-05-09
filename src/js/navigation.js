1f7556f9e23fa614ac01bca37bcf7acb79bd1378
petrahagman <phagman@Ps-MacBook-Pro.local>
2017-05-09 14:59:20 +0200

    Markerat aktiv section i scrollbar:en

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,38 +1,35 @@
 // Select all links with hashes
 $('a[href*="#"]')
-// Remove links that don't actually link to anything
+// Remove empty links
 .not('[href="#"]')
 .not('[href="#0"]')
 .click(function(event) {
-    // On-page links
     if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
         && 
         location.hostname == this.hostname
     ) {
-        // Figure out element to scroll to
+        // Find element to scroll to
         var target = $(this.hash);
-
         var targetName = target[0].id;
+
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        console.log('nhej');
         
-        // update url
+        // Update url
         if (window.history.replaceState) {
-           //prevents browser from storing history with each change:
+           //Prevents browser from storing history with each change:
            window.history.replaceState(targetName, targetName, '/' + targetName);
         }
-        
-        
+
+        // Add/remove class for active section
+        var mId = $(this).attr('class').substring(-1,7);
         $("a").removeClass('active');
         if ( !$( this ).hasClass( 'active' ) ) {
-            $("a").addClass('active');
+            $("a."+mId).addClass('active');
         }
         $(this).siblings().removeClass('active');
         $(this).toggleClass('active');
-        });
     
-        
         // Does a scroll target exist?
         if (target.length) {
             // Only prevent default if animation is actually gonna happen
@@ -44,13 +41,6 @@ $('a[href*="#"]')
                 // Must change focus!
                 var $target = $(target);
                 $target.focus();
-                
-                // if ($target.is(":focus")) { // Checking if the target was focused
-                //     return false;
-                // } else {
-                //     $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                //     $target.focus(); // Set focus again
-                // };
             });
         }
     }
