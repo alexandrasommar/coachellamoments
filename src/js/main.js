50823b574415fd637ce1feaa7c99fffdf7f105a0
Alice Langerth <aice.a.langerth@gmail.com>
2017-05-10 15:40:49 +0200

    Style för coachella block

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+//Link to coachella official website
+
+const visitBtn = document.querySelector('#visit-coachella');
+
+visitBtn.addEventListener('click', function() {
+	window.location="https://www.coachella.com/";
+});
+
