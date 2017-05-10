b6bb6e089751f0efa4e5916b5631c277c837a1a4
jessica <jessican.niemi@gmail.com>
2017-05-10 09:03:55 +0200

    bytt tab mot spaces

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,47 +1,47 @@
 (function(){
 
-	const mostRecent = document.querySelector('.most-recent .posts');
-	const topList = document.querySelector('.top-list .posts');
-	const jsonTopList = 'src/json/toplist.json';
-	const jsonMostRecent = 'src/json/mostrecent.json';
+    const mostRecent = document.querySelector('.most-recent .posts');
+    const topList = document.querySelector('.top-list .posts');
+    const jsonTopList = 'src/json/toplist.json';
+    const jsonMostRecent = 'src/json/mostrecent.json';
 
-	// Fetch request file
-	function fetchRequest() {
-		fetch('request.php')
-		.catch(function(error) {
-			console.log('There has been a problem with your fetch operation: ' + error.message);
-		});
-	}
+// Fetch request file
+function fetchRequest() {
+    fetch('request.php')
+        .catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
 
-	// Fetch results from json files
-	function fetchResults(filename, element) {
-		fetch(filename)
-		.then(function(result) {
-			return result.json();
-		}).then(function(result) {
-			console.log(result);
-			displayResults(result, element);
-		});
-	}
+    // Fetch results from json files
+    function fetchResults(filename, element) {
+        fetch(filename)
+        .then(function(result) {
+            return result.json();
+        }).then(function(result) {
+            console.log(result);
+            displayResults(result, element);
+        });
+    }
 
-	//Display result in HTML element
-	function displayResults(result, element) {
+    //Display result in HTML element
+    function displayResults(result, element) {
 
-		for(var i = 0; i < 5; i++) {
-			element.innerHTML += `<a href="https://instagram.com/p/${result[i].code}"><img src="${result[i].thumbnail_src}"></a>`;
-			element.innerHTML += `<p>${result[i].likes.count}</p>`;
-		}
-	}
+        for(var i = 0; i < 5; i++) {
+            element.innerHTML += `<a href="https://instagram.com/p/${result[i].code}"><img src="${result[i].thumbnail_src}"></a>`;
+            element.innerHTML += `<p>${result[i].likes.count}</p>`;
+        }
+    }
 
-	fetchRequest();
-	fetchResults(jsonTopList, topList);
-	fetchResults(jsonMostRecent, mostRecent);
-	
-	setTimeout(function() {
-		fetchRequest();
-		fetchResults(jsonTopList, topList);
-		fetchResults(jsonMostRecent, mostRecent);
-	},(1000 * 60 * 5) );
+    fetchRequest();
+    fetchResults(jsonTopList, topList);
+    fetchResults(jsonMostRecent, mostRecent);
+
+    setTimeout(function() {
+        fetchRequest();
+        fetchResults(jsonTopList, topList);
+        fetchResults(jsonMostRecent, mostRecent);
+    },(1000 * 60 * 5) );
 
 
 })();
