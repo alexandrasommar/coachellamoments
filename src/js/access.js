7d485edc6effa599e84ce7a18763aa2e80065bac
jessica <jessican.niemi@gmail.com>
2017-05-10 11:51:28 +0200

    inlogg till insta

---
+++ src/js/access.js
@@ -0,0 +1,47 @@
+(function($) {
+    'use strict'
+    
+    function getAccessToken() {
+        var cookies = '; '+ document.cookie;
+        var parts = cookies.split('; access_token=');
+        var token = parts.length === 2 ? parts.pop().split(';').shift() : null;
+        return token;
+    }
+    
+    function getImages() {
+        var url = "https://api.instagram.com/v1/users/self/media/recent/?";
+        var accessToken = getAccessToken();
+        
+        $.ajax( {
+            url: url
+            ,data: {
+                access_token:accessToken
+            }
+            ,dataType: 'jsonp'
+            ,success: function(resp) {
+                //console.log(response);
+                
+                for(var i in resp.data) {
+                    var img = $('<img>');
+                    img.attr('src', resp.data[i].images.low_resolution.url );
+                    $('#pictures').append(img);
+                }
+                
+            }
+        });
+    };
+    
+    $(function(){
+        // är användaren authentiserad mot instagram?
+         if (getAccessToken()) {
+            // Det finns en token
+            getImages();
+            $('#login').hide();
+         } else {
+             // Användaren har inte authentiserat
+            $('#login').show();
+         } ;
+    });
+    
+    
+}(jQuery));
