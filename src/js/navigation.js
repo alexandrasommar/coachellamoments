d6adbed6626f088d324fcf1c74c8de9c8ef8ce9a
petrahagman <phagman@Ps-MacBook-Pro.local>
2017-05-10 14:01:20 +0200

    Lite styling och påbörjad js för scroll, obs! ej klar

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -9,10 +9,10 @@ $('a[href*="#"]')
         && 
         location.hostname == this.hostname
     ) {
+
         // Find element to scroll to
         var target = $(this.hash);
         var targetName = target[0].id;
-
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         
         // Update url
@@ -21,18 +21,19 @@ $('a[href*="#"]')
            window.history.replaceState(targetName, targetName, '/' + targetName);
         }
 
-        // Add/remove class for active section
+        // Highlight active section
         var mId = $(this).attr('class').substring(-1,7);
+        
         $("a").removeClass('active');
-        if ( !$( this ).hasClass( 'active' ) ) {
+        
+        if (!$( this ).hasClass('active')) {
             $("a."+mId).addClass('active');
         }
+        
         $(this).siblings().removeClass('active');
         $(this).toggleClass('active');
-
-        //TODO: Fixa så att 'active' tas bort för alla sections vid klick på "to top"
     
-        // Does a scroll target exist?
+        // Check if scroll target exists
         if (target.length) {
             // Only prevent default if animation is actually gonna happen
             event.preventDefault();
@@ -46,7 +47,74 @@ $('a[href*="#"]')
             });
         }
     }
-// $('a.to-top').click(function(){
+});
+
+//Highlight active section on scroll
+// var home = $('#home').scrollTop();
+// var info = $(window).scrollTop();
+// var topList = $('#top-list').scrollTop();
+// var coachella = $('#coachella').scrollTop();
+// var mostRecent = $('#most-recent').scrollTop();
+
+// $(window).scroll(function() {
+
+//     if(info >= 1000) {
+//         $('#info').addClass('active');
+//     }
 
 // });
-});
+
+//$(window).scroll(function() {
+
+    // var scroll = $(window).scrollTop();
+    //var windowHeight = $(window).height();
+
+    // if (scrollPosition >= windowHeight) {
+    //     $('a[href*="#"]').removeClass('active');
+    //     $('section[class*="active"]').removeClass('active');
+    //     $('#info-btn').addClass('active');
+    //     $('#info').addClass('active');
+    // } else {
+    //     $('#info-btn').removeClass('active');
+    //     $('#info').removeClass('active');
+    // }
+
+//});
+
+// var scrollPosition = $('section.active').offset({top:0, left:0});
+
+//TEST
+
+// var scrollPosition = $('section.active').offset().top;
+// var screenHeight = $(window).height();
+// var activationOffset = 0;
+// var maxScrollHeight = $('body').height() + screenHeight;
+// //var scrollActivationPoint = scrollPosition - (screenHeight * activationOffset);
+// var scrollActivationPoint = screenHeight * 0.7;
+
+// $(window).scroll(function() {
+    
+//     var scrollPosY = window.pageYOffset;
+//     var elementInView = scrollPosY > scrollActivationPoint;
+//     var bottomOfPage = maxScrollHeight <= scrollPosY && !elementInView;
+
+//     if(elementInView || bottomOfPage) {
+            
+//         $('section.active').next('section').addClass('active');
+
+//         // elementInView.next('.new-page').addClass('active');
+//         // elementInView.prev('new-page.active').removeClass('active');
+
+//         $('a.active').next('a').addClass('active');
+
+//         //$('section').addClass('active');
+//         //$('a[href*="#"]').addClass('active');
+//     }
+
+// });
+
+
+
+
+
+
