c25d08aec408aa3daa82a4400b488307baae2661
Alexandra Sommar <alexandra@sommar.se>
2017-05-20 10:35:02 +0200

    Lägger till mer info till popupen samt om en trycker på esc stängs den

--- src/js/posts.js
+++ src/js/posts.js
@@ -23,7 +23,7 @@
       });
   }
 
-  // get random number based on max & min
+  // Get random number based on max & min
   function getRandomInt(min, max, unit) {
       return Math.floor(Math.random() * (max - min + 1)) + min + unit;
   }
@@ -32,32 +32,28 @@
     return (number + (2.5 * (Math.random()+1)) + 's');
   }
 
-  // set animation duration based on image height
+  // Set animation duration and z-index based on image height
   function speed(height, images) {
-      // var animationDur = 'animation-duration';
       if (height < 190) {
           images.style.animationDuration = aLittleBitRandomness(36);
           images.style.animationDelay = '-3s';
           images.style.zIndex = '2';
-          console.log(images.style);
       } else if (height > 190 && height < 230) {
           images.style.animationDuration = aLittleBitRandomness(32);
           images.style.zIndex = '3';
-          console.log(images.style);
       } else if (height > 230 && height < 270) {
           images.style.animationDuration = aLittleBitRandomness(28);
           images.style.animationDelay = '-6s';
           images.style.zIndex = '4';
-          console.log(images.style);
       } else {
           images.style.animationDuration = aLittleBitRandomness(24);
           images.style.animationDelay = '-9s';
           images.style.zIndex = '5';
-          //console.log(images);
       }
   }
-
+  // Save info about img in array for later use
   var moreInfo = [];
+
   //Display result in HTML element
   function displayResults(result, element) {
 
@@ -80,7 +76,6 @@
       for(let i = 0; i < 9; i++) {
 
           element.innerHTML += `<div class="single">
-                                  <!--<a href="https://instagram.com/p/${result[i].code}">!-->
                                   <img src="${result[i].thumbnail_src}">
                                   <div class="content">
                                     <i class="fa fa-heart" aria-hidden="true"></i>
@@ -90,7 +85,7 @@
 
           imgArr = element.querySelectorAll('.single');
           currentImg = imgArr[i];
-          moreInfo.push([result[i].thumbnail_src, result[i].caption, result[i].comments.count]);
+          moreInfo.push([result[i].thumbnail_src, result[i].caption, result[i].comments.count, result[i].code]);
           currentImg.style.height = getRandomInt(options.height.min, options.height.max, options.height.unit);
           currentImg.style.marginTop = getRandomInt(options.margin.min, options.margin.max, options.margin.unit);
           currentImg.style.marginLeft = getRandomInt(options.margin.min, options.margin.max, options.margin.unit);
@@ -99,23 +94,40 @@
       }
 
   }
-  console.log(moreInfo);
 
-  // Display more info
+  // Choose neccessary elements for pop up window
+  var popupDiv = document.querySelector('.pop-up');
+  var overlay = document.querySelector('.overlay');
+
+  // Display more info in pop up window
   function showImageContent() {
     var popupImages = document.querySelectorAll('.single');
     for(let i = 0; i < popupImages.length; i++) {
       popupImages[i].addEventListener('click', function() {
         var img = popupImages[i].querySelector('img');
-        var popupDiv = document.querySelector('.pop-up');
+        overlay.className = 'overlay visible';
         popupDiv.appendChild(img);
         if(img.src === moreInfo[i][0]) {
-          popupDiv.innerHTML += `<p>Caption: ${moreInfo[i][1]}</p>`;
+          popupDiv.innerHTML += `<p>Caption: ${moreInfo[i][1]}</p>
+                                <a href="https://instagram.com/p/${moreInfo[i][3]}" target="_blank">View on Instagram</a>
+                                <p>Comments: ${moreInfo[i][2]}</p>`;
         }
       });
     }
   }
 
+  // If esc key is pressed, close pop up window
+  // TO DO: MAKE IT CLOSE ON CLICK AS WELL
+  document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+      var overlay = document.querySelector('.overlay');
+      overlay.className = 'overlay';
+      popupDiv.innerHTML = '';
+    }
+  };
+
+  // Make sure elements are loaded
   function waitForElement(className, callback){
     var poops = setInterval(function(){
         if(document.querySelectorAll(className)){
