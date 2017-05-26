cac28821d7f9db12c35ce3747b3dd488b4b3e96d
jessica <jessican.niemi@gmail.com>
2017-05-26 13:18:15 +0200

    kommenterat i auth, som inte används

--- php/auth.php
+++ php/auth.php
@@ -1,4 +1,9 @@
 <?php 
+/*
+ Låter användaren godkänna vår sida och logga in på instagram om hen ej är inloggad.
+ Denna fil används dock inte i dagsläget då vi inte kunde använda Instagrams API som 
+ vi från början ville i sandbox mode. 
+*/
 
 $code = isset($_GET['code']) ? $_GET['code'] : null;
 $redirectURI = "http://localhost:3000/php/auth.php";
@@ -10,8 +15,6 @@ $clientSecret = "c0563d3400fb49e3a5480e6010bdf0d4";
 $jsonFile = "../json/mostrecent.json";
 $data = json_decode(file_get_contents($jsonFile));
 $test = $data[0]->id;
-//var_dump($test);
-//var_dump($data);
 
 if (is_null($code)) {
     $authURL = "https://api.instagram.com/oauth/authorize/?client_id=$clientID&redirect_uri=$redirectURI&response_type=code";  
@@ -37,10 +40,7 @@ if (is_null($code)) {
     
     $accessT = $response->access_token;
     
-    //error_log($response);
     error_log(print_r( $response, true ));
-    
-    // till för like-knappen
     
     $params = array(
         'access_token' => $accessT,
@@ -57,15 +57,11 @@ if (is_null($code)) {
     curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $params);
     curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER, true);
     $response = json_decode(curl_exec($curlHandler));
-    //var_dump($response);
     
     error_log(print_r( $response, true ));
     
-    
-    
-    
     setcookie('access_token', $response->access_token);
-//    echo "FOO: ".$response->access_token;
+
     header("location: /");
     
 }
